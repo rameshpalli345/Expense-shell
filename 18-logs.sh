8cc3596ec1844bc3740484ad11ce6abd4695634c
ramesh <rameshpalli@hotmail.com>
2024-09-08 23:14:59 +0200

    backend script

--- 18-logs.sh
+++ 18-logs.sh
@@ -15,8 +15,8 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +1 )
 echo "files: $FILES"
 while IFS= read -r line #IFS = internal feed seperator and r emtpy white spaces 
 do
-echo "delete the line : $line"
+echo "deleting the line : $line"
 rm -rf $line
-
+echo "deleted the $line"
 done <<< $FILES
 
