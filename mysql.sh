47a096521402d06142f5683d11b4aed0934c9385
ramesh <rameshpalli@hotmail.com>
2024-09-08 16:31:01 +0200

    backend script

--- mysql.sh
+++ mysql.sh
@@ -1,47 +1,59 @@
+
 #!/bin/bash
-LOGS_FOLDER="/var/log/shell-script"
+
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(date +%y-%m-%d-%H-%M)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
+
 USERID=$(id -u)
-#echo " print the value $USERID"
-CHECK_ROOT() {
-    if [ $USERID -ne 0 ]
-    then 
-   echo -e "$R User not have previlleged access stop execute the script $N" |tee -a $LOG_FILE
-   exit 1
-   fi
-}
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo -e "$1 is ..$R installation failed $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
     else
-    echo -e "$1 is ..$G installation sucess $N" | tee -a $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
- CHECK_ROOT
- dnf install mysql-server -y & >>LOG_FILE
- VALIDATE $? "install the my sql server"
- systemctl enable mysqld & >>LOG_FILE
- VALIDATE $? "check symlink connected"
- systemctl start mysqld & >>LOG_FILE
- VALIDATE $? "Check my sql service started"
- mysql -h  mysql.crazymonk.online  -u root -pExpenseApp@1 -e 'show databases;'   &  >>LOG_FILE
- 
- if [ $? -ne 0 ]
- then
-    echo "my sql root password not set up , setting now"
-    mysql_secure_installation --set-root-pass ExpenseApp@1 | tee -a $LOG_FILE
-    VALIDATE $? " set the root account password"
- else
-    echo -e "$Y mysql root password already setup $N"
-    
- fi
- 
- 
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
+
+mysql -h mysql.crazymonk.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
+
+# Assignment
+# check MySQL Server is installed or not, enabled or not, started or not
+# implement the above things
