93400d2278e5e4f9081e99a07276ab60ed2114f7
ramesh <rameshpalli@hotmail.com>
2024-09-08 16:06:55 +0200

    backend script

--- backend.sh
+++ backend.sh
@@ -1,66 +1,89 @@
 #!/bin/bash
-LOGS_FOLDER="/var/log/shell-script"
+
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(date +%y-%m-%d-%H-%M)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
+
 USERID=$(id -u)
-#echo " print the value $USERID"
-CHECK_ROOT() {
-    if [ $USERID -ne 0 ]
-    then 
-   echo -e "$R User not have previlleged access stop execute the script $N" |tee -a $LOG_FILE
-   exit 1
-   fi
-}
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo -e "$1 is ..$R installation failed $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
     else
-    echo -e "$1 is ..$G installation sucess $N" | tee -a $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
- CHECK_ROOT
- dnf module disable nodejs -y &>>$LOG_FILE
- VALIDATE $? "disable nodejs previous version"
- dnf module enable nodejs:20 -y &>>$LOG_FILE
- VALIDATE $? "enable nodejs version20"
 
- id expense &>>$LOG_FILE
- if [ $? -ne 0 ]
- then 
- "echo -e  expense user not exit .$G.creating $N"
- useradd expense & >>$LOG_FILE
- VALIDATE $? "user expense created"
- else
- echo -e  "expense user already created.. $Y skipping..$N"
- fi
- mkdir -p /app & >>$LOG_FILE
- VALIDATE $? " Creating new directory"
- curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
- VALIDATE $? "copy expense code temp directory"
- cd /app
- rm -rf /app/* #remove the exiting code
- unzip /tmp/backend.zip & >>$LOG_FILE
- VALIDATE $? "change directoy and extracting the code"
- npm install &>>$LOG_FILE
- VALIDATE $? " install the dependencies" 
- cp /root/Expense-shell/backend.service /etc/systemd/system/backend.service 
- dnf install mysql -y  &>>$LOG_FILE
- VALIDATE $? " install my sql client"
- mysql -h mysql.crazymonk.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
- VALIDATE $? "connection to backend schema"
- systemctl daemon-reload &>>$LOG_FILE
- VALIDATE $? "system load the sevices"
- systemctl enable backend &>>$LOG_FILE
- VALIDATE "enable the backend service"
- systemctl restart backend &>>$LOG_FILE
- VALIDATE $? "restart the backend services"
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install nodejs"
+
+id expense &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "expense user not exists... $G Creating $N"
+    useradd expense &>>$LOG_FILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "expense user already exists...$Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+cd /app
+rm -rf /app/* # remove the existing code
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# load the data before running backend
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.crazymonk.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
+
 
 
  
