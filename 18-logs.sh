6ba559df26f2b66343583639c6342496ba2d4de3
ramesh <rameshpalli@hotmail.com>
2024-09-08 22:50:22 +0200

    backend script

---
+++ 18-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+if [ -d $SOURCE_DIR ]
+then
+echo -e " $G SOURCE Directory exists..$R "
+else 
+echo -e " $R SOURCE Directory not exists..$N"
+fi
+
