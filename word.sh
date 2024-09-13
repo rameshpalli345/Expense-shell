236013e45f1217e0c8cb8f3c40f5f85be89ae065
ramesh <rameshpalli@hotmail.com>
2024-09-13 17:17:56 +0200

    backup.sh

--- word.sh
+++ word.sh
@@ -12,7 +12,7 @@ USAGE(){
   fi
 
 #count the number of words in given file
-number_words= wc --word <$SOURCE_FILE
+number_words="wc --word <$SOURCE_FILE"
 # Display the number of the words in the file
 echo "number of the words in the file"$number_words
 
