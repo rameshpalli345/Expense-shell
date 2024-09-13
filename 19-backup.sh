0000072d65350e08f0640f95399060e39c42c88d
ramesh <rameshpalli@hotmail.com>
2024-09-13 15:29:04 +0200

    backup.sh

--- 19-backup.sh
+++ 19-backup.sh
@@ -2,6 +2,7 @@
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # if $3 is empty then 14 days by default
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -27,9 +28,25 @@ Y="\e[33m"
        echo " Destination directory does not exit . please check."
   fi
 FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-if [ ! -z $FILES]
+if [ ! -z $FILES] # true if the files is empty ! nakes it expression is false
  then 
   echo "files are found "
+  ZIP_FILE="$DEST_DIR/applogs-$TIMESTAMP.zip"
+  find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
+  # check if the zip correctly woking
+  if [ -f $ZIP_FILE]
+    then
+   echo "succesfully zipped the logs older than $DAYS"
+   while IFS= read -r file #IFS = internal feed seperator and r emtpy white spaces 
+    do
+    echo "deleting the line : $file"
+    rm -rf $file
+    echo "deleted the $file"
+    done <<< $FILES
+    else 
+    echo " files are not zipped"
+    exit 1
+    fi
  else
   echo "no files older than $DAYS"
 fi
