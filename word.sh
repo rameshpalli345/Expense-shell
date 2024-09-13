f0d6faf4a480065bbd440311707c384f7bfb791e
ramesh <rameshpalli@hotmail.com>
2024-09-13 17:25:34 +0200

    backup.sh

--- word.sh
+++ word.sh
@@ -2,8 +2,10 @@
 SOURCE_FILE="/home/ec2-user/logs/filename.txt"
 #count the number of words in given file
 Number_words=$(wc --word <$SOURCE_FILE)
+Number_lines=$( wc --lines <$SOURCE_FILE)
 #Display no of words in the file
 echo " Number of words in the file = $Number_words "
+echo " Number of lines in the file = $Number_lines "
 
 
 
