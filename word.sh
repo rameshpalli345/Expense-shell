660cbfb18e4eb644ec55415c2dd5d36d2448410e
ramesh <rameshpalli@hotmail.com>
2024-09-13 17:19:42 +0200

    backup.sh

--- word.sh
+++ word.sh
@@ -1,18 +1,7 @@
 #!\bin\bash
-SOURCE_FILE=$1
-USAGE(){
-     echo -e  " $R USAGE:: $N sh word.sh filepath/filename"
-   }
-
- if [ ! -d $SOURCE_FILE ]
-    then  
-       echo " Source file does not exit ... please check"
-       USAGE
-       exit 1
-  fi
-
+SOURCE_FILE="/home/ec2-user/logs/filename.txt"
 #count the number of words in given file
-number_words="wc --word <$SOURCE_FILE"
+number_words="wc --word<$SOURCE_FILE"
 # Display the number of the words in the file
 echo "number of the words in the file"$number_words
 
