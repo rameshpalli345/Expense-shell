b8986e7468115aef78b9344dc4bdf4b4c0942bf4
ramesh <rameshpalli@hotmail.com>
2024-09-13 16:39:40 +0200

    backup.sh

---
+++ diskuage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DISK_USAGE=$(df-hT | grep xfs)
+DISK_THERSHOLD=5
+
+while IFS= read -r line #IFS = internal feed seperator and r emtpy white spaces 
+do
+  USAGE=$(echo $line | grep xfs |awk -F  " " '{print $6F}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | grep xfs | awk -F "   " '{print $NF}')
+  if [ $USAGE -ge $DISK_THERSHOLD ]
+  then
+       echo "$PARTITION is more than $DISK_THERSHOLD current valu: $USAGE , please check"
+   fi
+   done <<< $DISK_USAGE
